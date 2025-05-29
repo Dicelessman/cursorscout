830deb1265d588b7b98f673d8af66cd0521ee09a
Dicelessman <diceless@gmail.com>
2025-05-29 16:08:22 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -3,9 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// La configurazione Firebase andrà inserita qui
+// La configurazione Firebase
 const firebaseConfig = {
-    // TODO: Inserire qui la configurazione Firebase
+    apiKey: "AIzaSyDAs-B2Jk6PvQTb_S_bYfAiTDaQHnSF2EQ",
+    authDomain: "repartapp-3f1a4.firebaseapp.com",
+    projectId: "repartapp-3f1a4",
+    storageBucket: "repartapp-3f1a4.firebasestorage.app",
+    messagingSenderId: "1025798533836",
+    appId: "1:1025798533836:web:c046ef07edf3669e7b8869"
 };
 
 // Initialize Firebase
